2b32a8605e05314d9c0dbe54e3e6bab8f34fa674
ArsenShnurkov <Arsen.Shnurkov@gmail.com>
2013-11-24 17:57:11 +0400

    simple IP address detection

--- Default.aspx
+++ Default.aspx
@@ -6,7 +6,24 @@
 </head>
 <body>
 	<form id="form1" runat="server">
-		<asp:Button id="button1" runat="server" Text="Click me!" OnClick="button1Clicked" />
+	
+		<div runat="server" id="PanelTelecomCompanyChooser1">
+		<br />
+		<asp:Label id="LocationLabel1" runat="server" />
+		</div>
+		
+		
+		<div runat="server" id="PanelBitcoinAddress1">
+		<br />
+		Please enter bitcoin address:
+		<asp:TextBox id="BitcoinAddress1" runat="server" Text="" />
+		<asp:Button id="button1" runat="server" Text="Go!" OnClick="button1Clicked" />
+		</div>
+
+		<div runat="server" id="PanelDone1">
+		<br />
+		New page <asp:HyperLink id="BitcoinUrl1" runat="server" /> was created, you shound receive an SMS with confirmation code, and enter it there
+		</div>
 	</form>
 </body>
 </html>
--- Default.aspx.cs
+++ Default.aspx.cs
@@ -2,14 +2,48 @@
 namespace githubtests
 {
 	using System;
+	using System.Text;
 	using System.Web;
 	using System.Web.UI;
 
 	public partial class Default : System.Web.UI.Page
 	{
+		protected void Page_Load (object sender, EventArgs args)
+		{
+			if (IsPostBack) {
+			} else {
+				//PanelBitcoinAddress1.Visible = false;
+				PanelDone1.Visible = false;
+			}
+			// Populate the first panel
+			StringBuilder finalLocation = new StringBuilder (1024);
+			string addr1 = HttpContext.Current.Request.UserHostAddress;
+			finalLocation.AppendFormat ("IP: {0}", addr1);
+			string addr2 = Request.ServerVariables ["HTTP_X_FORWARDED_FOR"];
+			if (!string.IsNullOrWhiteSpace (addr2)) {
+				finalLocation.AppendFormat (", Proxy: {0}", addr2);
+			}
+			using (var objClient = new System.Net.WebClient()) {
+				var strFile = objClient.DownloadString ("http://freegeoip.net/xml/" + addr1);
+
+				finalLocation.AppendFormat (", GeoProvider: {0}", strFile);
+				LocationLabel1.Text = finalLocation.ToString ();
+			}
+		}
 		public void button1Clicked (object sender, EventArgs args)
 		{
-			button1.Text = "You clicked me";
+			button1.Text = "done";
+			button1.Enabled = false;
+			PanelDone1.Visible = true;
+
+			string targetURL = GetWalletURL ();
+			BitcoinUrl1.Text = targetURL;
+			BitcoinUrl1.NavigateUrl = targetURL;
+		}
+		string GetWalletURL ()
+		{
+			UriBuilder ub = new UriBuilder(Request.Url.Scheme, Request.Url.Host, Request.Url.Port, BitcoinAddress1.Text);
+			return ub.ToString ();
 		}
 	}
 }
